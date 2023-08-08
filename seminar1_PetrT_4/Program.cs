54cc089a2579145be0c45683afb204faeeed848e
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-08-08 20:37:36 +0300

    add task8

---
+++ seminar1_PetrT_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите N: ");
+    String strN = Console.ReadLine();
+
+int N = int.Parse(strN);
+Console.Write("все чётные числа от 1 до N: ");
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write( i + " ");
+    }
+}

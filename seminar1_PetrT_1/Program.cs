dbcec50b06651fb878ccf588f490c9d4a87ae802
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-08-08 19:17:52 +0300

    add task2

--- seminar1_PetrT_1/Program.cs
+++ seminar1_PetrT_1/Program.cs
@@ -1,13 +1,16 @@
 ﻿// задача 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.WriteLine("Введите первое число: ");
-    int numbA = int.Parse(Console.ReadLine());
+    String strA = Console.ReadLine();
+    int numbA = int.Parse(strA);
+
     Console.WriteLine("Введите второе число: ");
-    int numbB = int.Parse(Console.ReadLine());
+    String strB = Console.ReadLine();
+    int numbB = int.Parse(strB);
+   
 if (numbA == numbB)
 {
     Console.WriteLine("Введите неодинаковые числа!");
     return;
 }
-
 
 Console.WriteLine(numbA > numbB ? "первое число больше второго числа" : "первое чиcло меньше второго числа");
